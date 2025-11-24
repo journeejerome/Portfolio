2462abbeae0eb454e796045372c64eabd8b4c73e
Jerome <jerome.journee@gmail.com>
2025-11-24 16:49:40 +0100

    Add burger menu

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const buttonScroll = document.getElementById("buttonScroll");
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
 function doSomething(scrollPos) {
   if (scrollPos != 0) {
     buttonScroll.style.opacity = "1";
+    hamMenu.style.opacity = "1";
   } else {
     buttonScroll.style.opacity = "0";
+    hamMenu.style.opacity = "0";
   }
 }
 
@@ -23,6 +28,11 @@ document.addEventListener("scroll", (event) => {
     ticking = true;
   }
 });
+
 buttonScroll.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
+});
+hamMenu.addEventListener('click',() => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
 });
